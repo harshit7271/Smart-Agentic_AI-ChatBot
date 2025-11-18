5fc26182a199e78a1121610eca0a0621870ef171
Harshit Singh <harshitsingh05893312@gmail.com>
2025-11-19 01:01:49 +0530

    Update agent_ai.py

--- agent_ai.py
+++ agent_ai.py
@@ -13,7 +13,7 @@ GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
 
 # Initialize LLM and Search Tool
-groq_llm = ChatGroq(model="llama-3.3-70b-versatile",  api_key=GROQ_API_KEY)
+groq_llm = ChatGroq(model="llama-3.3-70b-versatile")
 search_tool = TavilySearchResults(max_results=2)
 
 system_prompt = "Act as an AI chatbot who is smart and friendly"
@@ -21,8 +21,6 @@ system_prompt = "Act as an AI chatbot who is smart and friendly"
 def get_response_from_ai_agent(llm_id, query, allow_search, system_prompt, provider="Groq"):
     if provider != "Groq":
         raise ValueError("Currently only 'Groq' provider is supported.")
-    
-    # You can optionally recreate llm here using llm_id if needed, or use the pre-initialized one
     # llm = ChatGroq(model=llm_id, api_key=GROQ_API_KEY)
     agent = create_react_agent(
         model=groq_llm,
@@ -44,6 +42,7 @@ if __name__ == "__main__":
     test_query = "Tell me about the trends in crypto markets"
     reply = get_response_from_ai_agent("llama-3.3-70b-versatile", test_query, allow_search=True, system_prompt=system_prompt)
     print(reply)
+
 
 
 
